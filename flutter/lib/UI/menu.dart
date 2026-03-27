be5d6839dcbb898152bc49ad873030db47df9467
Guedon Anaelle <o22402381@or-iut-i002-f20.iut45.univ-orleans.fr>
2026-03-27 09:35:18 +0100

    debut page d'accueil + modif menu

--- flutter/lib/UI/menu.dart
+++ flutter/lib/UI/menu.dart
@@ -1,40 +1,61 @@
 import 'package:flutter/material.dart';
+import 'accueil.dart';
 
-var menu = Scaffold(
-  body: Row(
-    children: [
-      Container(
-        width: 200,
-        color: Colors.grey[100],
-        child: Column(
-          children: [
-            ListTile(
-              title: Text("Accueil"),
-              onTap: () {},
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const Accueil(),
+    const Center(child: Text("Vols")),
+    const Center(child: Text("Correspondances")),
+    const Center(child: Text("Aéroports")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(
+            width: 200,
+            color: Colors.grey[100],
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text("Accueil"),
+                  selected: _selectedIndex == 0,
+                  onTap: () => setState(() => _selectedIndex = 0),
+                ),
+                ListTile(
+                  title: const Text("Vols"),
+                  selected: _selectedIndex == 1,
+                  onTap: () => setState(() => _selectedIndex = 1),
+                ),
+                ListTile(
+                  title: const Text("Correspondances"),
+                  selected: _selectedIndex == 2,
+                  onTap: () => setState(() => _selectedIndex = 2),
+                ),
+                ListTile(
+                  title: const Text("Aéroports"),
+                  selected: _selectedIndex == 3,
+                  onTap: () => setState(() => _selectedIndex = 3),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text("Vols"),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("Correspondances"),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("Aéroports"),
-              onTap: () {},
-            ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: _pages[_selectedIndex], //affiche la page sélectionnée
+          ),
+        ],
       ),
-
-      VerticalDivider(width: 1),
-
-      Expanded(
-        child: Center(
-          child: Text("Contenu ici"),
-        ),
-      ),
-    ],
-  ),
-);
+    );
+  }
+}
